f4d237bce1bebb0df830731ee739fe42d055d8bb
albara taani <albarataani00@gmail.com>
2025-01-14 00:37:55 +0300

    add multiply and divide functions by khaled

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,11 @@
 public class Calculator {
-
+    
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public int divide(int a, int b) {
+        return a / b;
+    }
     public static void main(String[] args) {
     }
 }
